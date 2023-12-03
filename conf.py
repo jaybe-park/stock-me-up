4619dd7674dd9ef971539bbaf7aac036cf2b1ec6
jaybe-park <jaybe.park@kakaocorp.com>
2023-12-03 17:17:55 +0900

    Add get_url

--- conf.py
+++ conf.py
@@ -1,6 +1,11 @@
 import json
 
 
+def get_url(name: str):
+    urls = json.load(open('./urls.json'))
+    return urls[name]
+
+
 def get_key(name: str):
     keys = json.load(open('./keys.json'))
     return keys[name]
