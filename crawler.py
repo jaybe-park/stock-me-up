f8916f48f44bc11920e48bf01d5de224771231fa
jaybe-park <jaybe.park@kakaocorp.com>
2024-09-23 21:36:48 +0900

    get_company 추가

--- crawler.py
+++ crawler.py
@@ -1,9 +1,9 @@
 import json
 import logging
-from logging import DEBUG
 import requests
 from io import BytesIO
 import zipfile
+import time, random
 
 from lxml import etree
 
@@ -79,16 +79,15 @@ class DARTCrawler(BaseCrawler):
         
         url = 'corpCode.xml'
         param = {'crtfc_key': self.key}
-        self.logger.debug('Key Load Completed')
         
         res = requests.get(
             self.base_url + url,
             params=param
         )
-        self.logger.debug('GET corpCode Done - {res.status_code}')
+        self.logger.debug(f'GET corpCode Done - {res.status_code}')
         
         if res.status_code != 200:
-            self.logger.error(f'Response Failed - {res.status_code}')
+            self.logger.error(f'Response Failed : res.status_code -  {res.status_code}')
             return
         
         zip_file = BytesIO(res.content)
@@ -105,21 +104,95 @@ class DARTCrawler(BaseCrawler):
                 corp_code = l.find('corp_code').text
                 corp_name = l.find('corp_name').text
                 modify_date = l.find('modify_date').text
-                
-                curr_d = dict()
-                curr_d['corp_code'] = corp_code
-                curr_d['corp_name'] = corp_name
-                curr_d['stock_code'] = stock_code
-                curr_d['modify_date'] = modify_date
 
                 corp_codes.append((corp_code, corp_name, stock_code, modify_date))
+
         self.logger.debug('Corp Code Parsed')
                 
         self.conn.executemany('insert or replace into dart_corp_code values (?, ?, ?, ?)', corp_codes)
         self.conn.commit()
-        self.logger.debug('Corp Code Updated')
+        self.logger.debug('get_corp_code Done')
         
         return
+    
+    
+    def get_company(self) -> None:
+        '''
+        DART에 등록되어있는 기업의 개황정보를 가져옵니다
+        
+        dart_corp_code에 있는 업데이트 날짜(modify_date)와 dart_company에 있는 업데이트 날짜(modyfy_date)를 비교하여 업데이트 합니다
+        '''
+        self.logger.debug('get_company Started')
+        
+        url = 'company.json'
+        
+        cursor = self.conn.cursor()
+        query_to_find_target = '''
+        SELECT
+            dart_corp_code.corp_code ,
+            dart_corp_code.modify_date ,
+            dart_company.modify_date as dart_company_modify_date
+        FROM 
+            dart_corp_code
+            left join
+            dart_company 
+            on dart_corp_code.corp_code  = dart_company.corp_code
+        WHERE
+            dart_company.modify_date is null
+            or dart_corp_code.modify_date > dart_company.modify_date 
+        '''
+        cursor.execute(query_to_find_target)
+        targets = cursor.fetchall()
+        self.logger.debug('Getting Targets Done')
+        
+        query_to_insert = 'insert or replace into dart_company values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
+        
+        for corp_code, modify_date, __ in targets:
+            param = {'crtfc_key': self.key, 'corp_code': corp_code}
+            
+            res = requests.get(
+                self.base_url + url,
+                params=param
+            )
+            if res.status_code != 200:
+                self.logger.error(f'Response Failed : res.status_code -  {res.status_code}')
+                return
+            
+            data = res.json()
+            if data['status'] != '000':
+                self.logger.error(f'Response Failed : data[\'status\'] -  {data['status']}')
+                return
+            self.logger.debug(f'GET company Done (corp_code: {corp_code})')
+        
+            curr_d = list()
+            curr_d.append(corp_code)
+            curr_d.append(data['corp_name'])
+            curr_d.append(data['corp_name_eng'])
+            curr_d.append(data['stock_name'])
+            curr_d.append(data['stock_code'])
+            curr_d.append(data['ceo_nm'])
+            curr_d.append(data['corp_cls'])
+            curr_d.append(data['jurir_no'])
+            curr_d.append(data['bizr_no'])
+            curr_d.append(data['adres'])
+            curr_d.append(data['hm_url'])
+            curr_d.append(data['ir_url'])
+            curr_d.append(data['phn_no'])
+            curr_d.append(data['fax_no'])
+            curr_d.append(data['induty_code'])
+            curr_d.append(data['est_dt'])
+            curr_d.append(data['acc_mt'])
+            curr_d.append(modify_date)
+
+            self.conn.execute(query_to_insert, tuple(curr_d))
+            self.conn.commit()
+
+            self.logger.debug(f'company Updated (corp_code: {corp_code})')
+            
+            # Exceeding Request Rate 에러 발생으로 0~0.1초 사이 랜덤으로 sleep 로직 추가
+            time.sleep(random.random() / 10)
+            
+        self.logger.debug('get_company Done')
                 
                 
   
