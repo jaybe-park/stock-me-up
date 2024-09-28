74cc7f526f0ba604d8bbc4bef26d2df32f0e2f2e
jaybe-park <jaybe.park@kakaocorp.com>
2024-09-28 13:06:07 +0900

    get_key 함수 추가

--- util.py
+++ util.py
@@ -1,5 +1,5 @@
 import sqlite3
-
+import json
 
 def get_database_connection() -> sqlite3.Connection:
     '''
@@ -10,3 +10,11 @@ def get_database_connection() -> sqlite3.Connection:
     )
     
     return conn
+
+
+def get_key(api_code) -> str:
+    '''
+    API 사용에 필요한 key를 가져옵니다
+    '''
+    key = json.loads(open('keys.json').read())[api_code]
+    return key
